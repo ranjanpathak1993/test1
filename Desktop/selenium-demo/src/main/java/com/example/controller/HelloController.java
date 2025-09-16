beef73eea32c2025626fbec717c076d4f8f7a166
ranjanpathak1993 <ranjanpathak@zenexah.com>
2025-09-16 14:35:44 +0530

    Fixed double quotes syntax in HelloController.java

--- Desktop/selenium-demo/src/main/java/com/example/controller/HelloController.java
+++ Desktop/selenium-demo/src/main/java/com/example/controller/HelloController.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @GetMapping("/")
     public String hello() {
         return "Hello World!";
-
     }
+
 }
